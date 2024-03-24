cca9f9c2642a364092813161340313fac2049eca
Venkatesh <Venkatesh111@gmail.com>
2024-03-24 22:25:21 +0530

    update ssm instance profile

--- 03_iam.tf
+++ 03_iam.tf
@@ -1,8 +1,3 @@
-resource "aws_iam_instance_profile" "ssm" {
-  name = "ssm-role-instance-profile"
-  role = aws_iam_role.ssm_role.id
-}
-
 resource "aws_iam_role" "ssm_role" {
   name               = "ssm-role"
   assume_role_policy = <<EOF
@@ -24,4 +19,9 @@ EOF
 resource "aws_iam_role_policy_attachment" "ssm_role_attachment" {
   role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm" {
+  name = "ssm-role-instance-profile"
+  role = aws_iam_role.ssm_role.id
 }
